b2080691d3a25adc5c3d00bbfa0c7f060bf970be
jsommer1738 <joel.sommer@gmail.com>
2018-03-23 00:59:07 -0400

    Done?
    To run this, you have to have a NVIDIA GPU and lots of RAM.  Made some optimizations to how memory was being used to help out.
    
    As is, this produces a score of: [0.12795436082035302, 1.0]

--- Source code/test5.py
+++ Source code/test5.py
@@ -1,6 +1,3 @@
-# Derived from using the source code at the blog post at
-# http://monik.in/a-noobs-guide-to-implementing-rnn-lstm-using-tensorflow/ as an example.
-
 import numpy as np
 import random
 from random import sample
@@ -14,25 +11,27 @@ from keras.layers import LSTM
 from keras.layers import CuDNNLSTM
 from keras.layers import Activation
 from keras import optimizers
+from keras.models import load_model
 
 
 def shuffle_data(input_data, output_data):
     print("Entering shuffle_data()")
-    zipped = []
+    zipped = np.empty([len(input_data)], dtype=tuple)
 
     print('len(input_data): {}'.format(len(input_data)))
     print('len(output_data): {}'.format(len(output_data)))
-
+    print('"zipping" data')
     for i in range(len(input_data)):
-        zipped.append((utils.chunk_data2(input_data[i]), output_data[i]))
+        zipped[i] = (utils.chunk_data2(input_data[i]), output_data[i])
+    print('zip complete')
+    np.random.shuffle(zipped)
+    print('shuffle complete')
 
-    zipped = sample(zipped, len(zipped))
-
-    train_input = []
-    train_output = []
+    train_input = np.empty([len(input_data), 32, 1])
+    train_output = np.empty([len(input_data), 1])
     for i in range(len(zipped)):
-        train_input.append(zipped[i][0])
-        train_output.append(zipped[i][1])
+        train_input[i] = np.asarray(zipped[i][0])
+        train_output[i] = np.asarray(zipped[i][1])
 
     print('------------------------------------------------------------------')
 
@@ -47,12 +46,14 @@ def shuffle_data(input_data, output_data):
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-NUM_TRAIN_PER_DFA_BIN = 10000
-NUM_TEST_PER_DFA_BIN = 10000
+NUM_TRAIN_PER_DFA_BIN = 1000000
+NUM_TEST_PER_DFA_BIN = 1000000
 DATA_WIDTH = 32
 TARGET_DFA = 'dfa_0'
 MAX_BATCH_SIZE = 3000
-EPOCHS = 500
+EPOCHS = 15000
+
+RELOAD_MODEL = True
 
 (train_x, train_y) = utils.load_train_data2(qty=NUM_TRAIN_PER_DFA_BIN)
 NUM_TRAIN = len(train_x)
@@ -88,41 +89,55 @@ for dfa_name in test_x:
 
 
 train_input, train_output = shuffle_data(train_y, train_output)
-test_input, test_output = shuffle_data(test_y, test_output)
-
 train_y = None
+
+test_input, test_output = shuffle_data(test_y, test_output)
 test_y = None
 
-train_input = np.asarray(train_input)
-train_output = np.asarray(train_output)
-test_input = np.asarray(test_input)
-test_output = np.asarray(test_output)
-
+# train_input = np.asarray(train_input)
+# train_output = np.asarray(train_output)
+# test_input = np.asarray(test_input)
+# test_output = np.asarray(test_output)
 
 print("Testing and training data loaded")
 
-model = Sequential()
-# model.add(Embedding(2, output_dim=5, input_shape=(3000, 32, 1)))
-# model.add(LSTM(1, input_shape=(32, 1), activation='hard_sigmoid', unit_forget_bias=True))
-model.add(CuDNNLSTM(1, input_shape=(32, 1), unit_forget_bias=True))
-# model.add(LSTM(1, input_shape=(32, 1), activation='tanh', unit_forget_bias=True))
-model.add(Activation('tanh'))
-# model.add(Activation('softmax'))
-# model.add(Dropout(0.5))
-# model.add(Dense(1, activation='sigmoid'))
 
-rmsprop = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
+if RELOAD_MODEL is True:
+    model = load_model('C:/Users/joels/Documents/GitHub/Autonomous_Detection_of_Regular_Language/Source code/model_data/model.h5')
+
+else:
+
+    model = Sequential()
+    # model.add(LSTM(1, input_shape=(32, 1), activation='hard_sigmoid', unit_forget_bias=True))
+    model.add(CuDNNLSTM(1, input_shape=(DATA_WIDTH, 1), unit_forget_bias=True))
+
+    # # print(model.layers[0].get_config())
+
+    # model.add(CuDNNLSTM(2, input_shape=(2, 1), unit_forget_bias=True))
+    model.add(Activation('tanh'))
+    # model.add(Activation('softmax'))
+    # model.add(Dropout(0.5))
+    # model.add(Dense(1, activation='sigmoid'))
+
+    rmsprop = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=None, decay=0.0)
 
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
-              metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy',
+                optimizer='rmsprop',
+                metrics=['accuracy'])
 
 
 print(len(train_input))
 print(len(train_output))
 
-model.fit(train_input, train_output, batch_size=MAX_BATCH_SIZE, epochs=EPOCHS)
-score = model.evaluate(test_input, test_output, batch_size=MAX_BATCH_SIZE)
 
-print(score)
+try:
+    # model.fit(train_input, train_output, batch_size=MAX_BATCH_SIZE, epochs=EPOCHS)
+    pass
+except KeyboardInterrupt as e:
+    pass
+finally:
+    if RELOAD_MODEL is False:
+        model.save('C:/Users/joels/Documents/GitHub/Autonomous_Detection_of_Regular_Language/Source code/model_data/model.h5')
+    score = model.evaluate(test_input, test_output, batch_size=MAX_BATCH_SIZE)
+    print(score)
